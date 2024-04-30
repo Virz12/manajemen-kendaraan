1526dc599db4145e1595d8fb06a99ad54987a7ae
Frmsfwn <hammerrmx1320@gmail.com>
2024-04-30 10:05:12 +0700

    Upload File Project Manajemen_Kendaraan
    30-04-2024-10:05 AM

---
+++ app/Models/detail_peminjaman.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class detail_peminjaman extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'nopol',
+        'id_pegawai',
+        'id_supir'
+    ];
+    protected $table = 'detail_peminjaman';
+}
