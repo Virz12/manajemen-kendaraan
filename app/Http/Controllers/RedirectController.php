15ce54b632bcffdf53eb401dbd5b3cdb9869d4e5
Frmsfwn <hammerrmx1320@gmail.com>
2024-05-01 21:33:10 +0700

    Update File Project Manajemen-Kendaraan

---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RedirectController extends Controller
+{
+    function admin()
+    {
+        return view('admin.dashboard_admin');
+    }
+
+    function pegawai()
+    {
+        return view('pegawai.homepage');
+    }
+
+    function kendaraan()
+    {
+        return view('kendaraan.dashboard_kendaraan');
+    }
+
+    function supir()
+    {
+        return view('supir.homepage');
+    }
+}
